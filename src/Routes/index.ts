9717e589b76bdaa0f950422087045ee2cfd72add
victor curilao <victor.edu22@gmail.com>
2023-01-30 02:53:26 -0300

    se agregan usevalidator al proyecto

--- src/Routes/index.ts
+++ src/Routes/index.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import UserController from "../Controllers/User.controller";
-// Old node way
-// const express = require('express');
-// const Router = express.Router;
+import validateUser from "../Controllers/User.validator";
+import { body, param, query, validationResult } from "express-validator";
+import { validate } from "../middleware/rejectInvalid";
+
 const router = Router();
-router.post("/user", UserController.CreateUser);
-router.get("/user", UserController.getUser);
-router.patch("/:id");
-router.delete("/:id");
+
+router.post(
+  "/user",
+  validate(validateUser.validateCreate),
+  UserController.CreateUser
+);
+router.get("/user", UserController.index);
+router.get("/:id", UserController.show);
+router.patch("/:id", UserController.upsert);
+
 export default router;
